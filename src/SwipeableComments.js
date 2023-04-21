85059bdaa1419900b8cd5aac35bb207fdca07a5c
cornstar23 <jessecorns@gmail.com>
2023-04-21 01:32:47 +0000

    refactored so more comments/replies logic is inside the SwipeableComments component.

--- src/SwipeableComments.js
+++ src/SwipeableComments.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SwipeableViews from "react-swipeable-views";
 import virtualize from "./virtualizeWithChildren";
 
@@ -11,30 +11,207 @@ const VirtualizeSwipeableViews = virtualize(SwipeableViews);
 
 const styles = {
   slideContainer: {
-    width: "calc(100% - 20px)"
-  }
+    width: "calc(100% - 20px)",
+  },
 };
 
-export function SwipeableComments({
-  comments,
-  moreCommentsMap,
+// MAX COMMENTS PER LEVEL
+const MAX = 5;
+// MAX COMMENT REPLIES PER COMMENT
+const MAX_DEPTH = 2;
+
+function buildCommentsMap(sourceComments) {
+  const levelCountMap = {};
+  const allCommentsMap = {};
+  const moreCommentsMap = {};
+  sourceComments.forEach((c) => {
+    levelCountMap[c.parentId] = (levelCountMap[c.parentId] || 0) + 1;
+    const level = Math.floor((levelCountMap[c.parentId] - 1) / MAX);
+
+    // TODO: Separate setting the moreCommentsMap
+    if ((levelCountMap[c.parentId] % MAX) / MAX === 0) {
+      const totalAtLevel = sourceComments.filter(
+        (c2) => c2.parentId === c.parentId
+      ).length;
+      const currentCount = (level + 1) * MAX;
+      const remainingAtLevel = totalAtLevel - currentCount;
+      if (remainingAtLevel !== 0) {
+        const toLoad = remainingAtLevel > MAX ? MAX : remainingAtLevel;
+        moreCommentsMap[c.id] = {
+          indexOfNextComments: level + 1,
+          toLoad,
+        };
+      }
+    }
+
+    if (!allCommentsMap[c.parentId]) {
+      allCommentsMap[c.parentId] = [];
+    }
+    if (!allCommentsMap[c.parentId][level]) {
+      allCommentsMap[c.parentId][level] = [];
+    }
+    allCommentsMap[c.parentId][level].push(c);
+  });
+  return { allCommentsMap, moreCommentsMap };
+}
+
+function loadComments(allCommentsMap, id) {
+  const targetComments = [];
+  const moreCommentRepliesMap = {};
+  if(!!allCommentsMap?.[id]?.[0]) {
+    loadCommentsRecursive(
+      allCommentsMap,
+      allCommentsMap[id][0],
+      targetComments,
+      moreCommentRepliesMap,
+      0,
+      true
+    );
+  }
+  return { targetComments, moreCommentRepliesMap };
+}
+
+function loadComments2(allCommentsMap, id) {
+  const targetComments = [];
+  if(!!allCommentsMap?.[id]?.[0]) {
+  loadCommentsRecursive2(
+    allCommentsMap,
+    allCommentsMap[id][0],
+    targetComments,
+    0,
+    true
+  );
+  }
+  return targetComments;
+}
+
+function loadCommentsRecursive(
+  allCommentsMap,
+  sourceComments,
+  targetComments,
   moreCommentRepliesMap,
-  getMoreComments,
-  getMoreReplies
-}) {
+  level,
+  saveComment
+) {
+  sourceComments.forEach((c) => {
+    if (saveComment) {
+      targetComments.push(c);
+    }
+    if (allCommentsMap[c.id]) {
+      if (level < MAX_DEPTH - 1) {
+        loadCommentsRecursive(
+          allCommentsMap,
+          allCommentsMap[c.id][0],
+          targetComments,
+          moreCommentRepliesMap,
+          level + 1,
+          saveComment
+        );
+      } else if (level === MAX_DEPTH - 1) {
+        moreCommentRepliesMap[c.id] = {
+          indexOfNextComments: level + 1,
+          numOfChildren: getChildrenDepth(allCommentsMap, c),
+        };
+        loadCommentsRecursive(
+          allCommentsMap,
+          allCommentsMap[c.id][0],
+          targetComments,
+          moreCommentRepliesMap,
+          0,
+          false
+        );
+      }
+    }
+  });
+}
+
+function loadCommentsRecursive2(
+  allCommentsMap,
+  sourceComments,
+  targetComments,
+  level,
+  saveComment
+) {
+  sourceComments.forEach((c) => {
+    if (saveComment) {
+      targetComments.push(c);
+    }
+    if (allCommentsMap[c.id]) {
+      if (level < MAX_DEPTH - 1) {
+        loadCommentsRecursive2(
+          allCommentsMap,
+          allCommentsMap[c.id][0],
+          targetComments,
+          level + 1,
+          saveComment
+        );
+      } else if (level === MAX_DEPTH - 1) {
+        loadCommentsRecursive2(
+          allCommentsMap,
+          allCommentsMap[c.id][0],
+          targetComments,
+          0,
+          false
+        );
+      }
+    }
+  });
+}
+
+function getChildrenDepth(allCommentsMap, comment) {
+  if (allCommentsMap[comment.id]) {
+    return (
+      1 + getChildrenDepth(allCommentsMap, allCommentsMap[comment.id][0][0])
+    );
+  }
+  return 0;
+}
+
+export function SwipeableComments({ comments }) {
   const commentMap = {};
   const [hashIndex, setHashIndex] = useState({});
+  const [displayedComments, setDisplayedComments] = useState([]);
+  const [allCommentsMap, setAllCommentsMap] = useState({});
+  const [moreCommentsMap, setMoreCommentsMap] = useState({});
+  const [moreCommentRepliesMap, setMoreCommentRepliesMap] = useState({});
+
+  useEffect(() => {
+    const { allCommentsMap: am, moreCommentsMap: mcm } =
+      buildCommentsMap(comments);
+    const parentId = "";
+    const { targetComments, moreCommentRepliesMap: mcrm } = loadComments(am, parentId);
+    setAllCommentsMap(am);
+    setMoreCommentsMap(mcm);
+    setDisplayedComments(targetComments);
+    setMoreCommentRepliesMap(mcrm);
+  }, [comments]);
 
   const _setHashIndex = (id, index) => {
     setHashIndex({ ...hashIndex, [id]: index });
   };
+
+  function getMoreComments(pId, indexOfNextComments) {
+    console.log("loading more comments");
+    setDisplayedComments([
+      ...displayedComments,
+      ...allCommentsMap[pId][indexOfNextComments],
+    ]);
+  }
+
+  function getMoreReplies(id) {
+    console.log("loading more replies: ", id);
+    const newComments = loadComments2(allCommentsMap, id);
+    setDisplayedComments([...displayedComments, ...newComments]);
+  }
 
   const commentTree = (parentId, level, parentVisible) => {
     const currentIndex = hashIndex[parentId] || 0;
 
     let commentsAtThisLevel = commentMap[parentId];
     if (!commentsAtThisLevel) {
-      commentsAtThisLevel = comments.filter(c => c.parentId === parentId);
+      commentsAtThisLevel = displayedComments.filter(
+        (c) => c.parentId === parentId
+      );
       commentMap[parentId] = commentsAtThisLevel;
     }
 
@@ -45,7 +222,8 @@ export function SwipeableComments({
         level={level}
         moreCommentRepliesMap={moreCommentRepliesMap[comment.id]}
         isLastVisibleComment={
-          comments.filter(c => c.parentId === comment.id).length === 0
+          displayedComments.filter((c) => c.parentId === comment.id).length ===
+          0
         }
         getMoreReplies={getMoreReplies}
         displayChildren={i + 2 > currentIndex && i - 2 < currentIndex}
@@ -84,7 +262,7 @@ export function SwipeableComments({
                 max={commentsAtThisLevel.length}
                 value={currentIndex + 1}
                 onMouseDown={() => console.log("test click")}
-                onChange={e => _setHashIndex(parentId, e.target.value - 1)}
+                onChange={(e) => _setHashIndex(parentId, e.target.value - 1)}
                 className="slider"
                 id="myRange"
               />
@@ -129,7 +307,7 @@ export function SwipeableComments({
           )}
 
           <VirtualizeSwipeableViews
-            onChangeIndex={i => {
+            onChangeIndex={(i) => {
               _setHashIndex(parentId, i);
             }}
             index={hashIndex[parentId] || 0}
--- src/index.js
+++ src/index.js
@@ -4,129 +4,23 @@ import { SwipeableComments } from "./SwipeableComments.js";
 import { loadRedditComments } from "./data/redditParser.js";
 
 const allComments = require("./data/comments.json");
-const parentId = "";
-const allCommentsMap = {};
-
-const moreCommentsMap = {};
-const moreCommentRepliesMap = {};
-//   {
-//   "77e11830-20e7-46e6-b084-f8b257288e99": { indexOfNextComments: 1 }
-// };
-const initialComments = [];
-
-const levelCountMap = {};
-// MAX COMMENTS PER LEVEL
-const MAX = 5;
-// MAX COMMENT REPLIES PER COMMENT
-const MAX_DEPTH = 2;
-
-allComments.forEach(c => {
-  levelCountMap[c.parentId] = (levelCountMap[c.parentId] || 0) + 1;
-  const level = Math.floor((levelCountMap[c.parentId] - 1) / MAX);
-  if ((levelCountMap[c.parentId] % MAX) / MAX === 0) {
-    const totalAtLevel = allComments.filter(c2 => c2.parentId === c.parentId)
-      .length;
-    const currentCount = (level + 1) * MAX;
-    const remainingAtLevel = totalAtLevel - currentCount;
-    if (remainingAtLevel !== 0) {
-      const toLoad = remainingAtLevel > MAX ? MAX : remainingAtLevel;
-      moreCommentsMap[c.id] = {
-        indexOfNextComments: level + 1,
-        toLoad
-      };
-    }
-  }
-
-  if (!allCommentsMap[c.parentId]) {
-    allCommentsMap[c.parentId] = [];
-  }
-  if (!allCommentsMap[c.parentId][level]) {
-    allCommentsMap[c.parentId][level] = [];
-  }
-  allCommentsMap[c.parentId][level].push(c);
-});
-
-const loadComments = (sourceComments, targetComments) => {
-  loadCommentsRecursive(sourceComments, targetComments, 0, true);
-};
-
-const loadCommentsRecursive = (
-  sourceComments,
-  targetComments,
-  level,
-  saveComment
-) => {
-  sourceComments.forEach(c => {
-    if (saveComment) {
-      targetComments.push(c);
-    }
-    if (allCommentsMap[c.id]) {
-      if (level < MAX_DEPTH - 1) {
-        loadCommentsRecursive(
-          allCommentsMap[c.id][0],
-          targetComments,
-          level + 1,
-          saveComment
-        );
-      } else if (level === MAX_DEPTH - 1) {
-        moreCommentRepliesMap[c.id] = {
-          indexOfNextComments: level + 1,
-          numOfChildren: getChildrenDepth(c)
-        };
-        loadCommentsRecursive(
-          allCommentsMap[c.id][0],
-          targetComments,
-          0,
-          false
-        );
-      }
-    }
-  });
-};
-
-const getChildrenDepth = (comment, level) => {
-  if (allCommentsMap[comment.id]) {
-    return 1 + getChildrenDepth(allCommentsMap[comment.id][0][0]);
-  }
-  return 0;
-};
-
-loadComments(allCommentsMap[parentId][0], initialComments);
 
 function App() {
-  const [comments, setComments] = useState(initialComments);
-  // useEffect(() => {
-  //   const test = async () => {
-  //     const c = await loadRedditComments();
-  //     setComments(c.commentList);
-  //   };
-  //   test();
-  //   // setComments(initialComments);
-  // }, []);
+  const [comments, setComments] = useState([]);
+  
+  useEffect(() => {
+    const test = async () => {
+      const c = await loadRedditComments();
+      setComments(c.commentList);
+    };
+    test();
+    // setComments(allComments);
+  }, []);
 
-  const getMoreComments = (pId, indexOfNextComments) => {
-    console.log("loading more comments");
-    setComments([...comments, ...allCommentsMap[pId][indexOfNextComments]]);
-  };
-
-  const getMoreReplies = id => {
-    console.log("loading more replies: ", id);
-    const newComments = [];
-    loadComments(allCommentsMap[id][0], newComments);
-    setComments([...comments, ...newComments]);
-  };
 
   return (
       <SwipeableComments
         comments={comments}
-        // moreCommentsMap={moreCommentsMap}
-        // moreCommentRepliesMap={moreCommentRepliesMap}
-        // getMoreComments={getMoreComments}
-        // getMoreReplies={getMoreReplies}
-        moreCommentsMap={{}}
-        moreCommentRepliesMap={{}}
-        getMoreComments={() => {}}
-        getMoreReplies={() => {}}
       />
   );
 }
