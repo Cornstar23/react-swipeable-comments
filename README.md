8a381542d4639affd4eb91d1a8bfa019e3bc1750
Jesse Corns <jessecorns@gmail.com>
2023-04-29 18:20:28 -0400

    Updated to be used as an npm package.

---
+++ README.md
@@ -0,0 +1,1 @@
+## React Swipeable Comments
---
+++ src/Comment.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Comment = ({
+  renderComment,
+  id,
+  level,
+  moreCommentRepliesMap,
+  isLastVisibleComment,
+  getMoreReplies,
+  displayChildren,
+  style,
+  children,
+}) => {
+  return (
+    <div className="comments" style={style}>
+      <div
+        className={`${level === 0 ? ' comment-top' : ''} comment comment-body`}
+      >
+        {renderComment()}
+      </div>
+      {isLastVisibleComment && moreCommentRepliesMap && (
+        <div className="loadMoreRepliesDiv">
+          <button
+            className="loadMoreComments"
+            onClick={() =>
+              getMoreReplies(
+                id,
+                moreCommentRepliesMap.indexOfNextComments,
+                moreCommentRepliesMap.maxDepth,
+              )
+            }
+          >
+            {moreCommentRepliesMap.numOfChildren} more replies...
+          </button>
+        </div>
+      )}
+      {displayChildren && children}
+    </div>
+  )
+}
+
+export default Comment
